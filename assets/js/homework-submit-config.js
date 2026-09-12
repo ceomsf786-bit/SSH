7f31a5df11235febe95542eb4a12f52103b2719c
ceomsf786-bit <ceomsf786@gmail.com>
2026-09-12 07:12:26 +0200

    Connect homework submission page to Apps Script backend

--- assets/js/homework-submit-config.js
+++ assets/js/homework-submit-config.js
@@ -1,6 +1,5 @@
 // SNT Homework Drive Submission MVP
-// Paste the deployed Google Apps Script Web App /exec URL here after deployment.
-window.SNT_HOMEWORK_UPLOAD_ENDPOINT = "";
+window.SNT_HOMEWORK_UPLOAD_ENDPOINT = "https://script.google.com/macros/s/AKfycbzxn-3qAUzrLdaHfeTxhVS1ecBddfFPcUlAEcUaImKwoULW37PPQ4WQsXl5c4h4boXRzw/exec";
 
 // Student-friendly limits. Final generated PDF is also checked before hand-in.
 window.SNT_HOMEWORK_MAX_IMAGES = 8;
