743a29e0c9238efa7493a1a9410569f445212457
ceomsf786-bit <ceomsf786@gmail.com>
2026-09-24 21:37:58 +0200

    Point homework uploader to Apps Script v4 deployment

--- assets/js/homework-submit-config.js
+++ assets/js/homework-submit-config.js
@@ -1,5 +1,5 @@
 // SNT Homework Drive Submission MVP
-window.SNT_HOMEWORK_UPLOAD_ENDPOINT = "https://script.google.com/macros/s/AKfycbzxn-3qAUzrLdaHfeTxhVS1ecBddfFPcUlAEcUaImKwoULW37PPQ4WQsXl5c4h4boXRzw/exec";
+window.SNT_HOMEWORK_UPLOAD_ENDPOINT = "https://script.google.com/macros/s/AKfycbyQftGanvhNYc3BavR-moP88WDt4G8yBAxaL-oJYzJddyCVHHHGBHxsyzZ5-WOnE-f_/exec";
 
 // Student-friendly limits. Final generated PDF is also checked before hand-in.
 window.SNT_HOMEWORK_MAX_IMAGES = 8;
